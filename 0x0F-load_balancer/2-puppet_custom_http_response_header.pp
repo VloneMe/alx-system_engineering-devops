1a8509bdcdf34149c88e4968b03c4b308ca4cd9b
Vlone Me <francisntima@gmail.com>
2023-10-05 12:12:22 +0000

    custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,58 +1,19 @@
 # Web server Configs with HTTP header response.
 
 # Install Nginx
-package { 'nginx':
-  ensure => 'installed',
+exec { "Installing nginx":
+	command => 'sudo apt-get -y update && sudo apt-get install -y nginx',
+	provider => shell
 }
 
-# Redirection Configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => "server {\n  server_name _;\n  rewrite ^/redirect_me https://github.com/VloneMe permanent;\n}",
-  notify  => Service['nginx'],
+# Add a custom HTTP header to the Nginx configuration
+exec { 'HTTP header Response':
+	command => 'sudo sed -i "/server_name _/a add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-enabled/default',
+	provider => shell
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-}
-
-# Create a custom 404 Not Found page
-file { '/var/www/html/custom_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page",
-}
-
-# Add "Hello, World!" to index.html
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello, World!',
-}
-
-# Configure Nginx to use the custom 404 page
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => "server {\n  listen 80 default_server;\n  error_page 404 /custom_404.html;\n  location = /custom_404.html {\n    root /var/www/html;\n    internal;\n  }\n}",
-  notify  => Service['nginx'],
-}
-
-# Create a custom HTTP response header
-file_line { 'custom_header':
-  path    => '/etc/nginx/sites-available/default',
-  line    => '    server_name _;',
-  match   => '    server_name _;',
-  after   => '    server_name _;',
-  content => '    add_header X-Served-By $HOSTNAME;',
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx to apply changes
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [
-    File['/etc/nginx/sites-available/default'],
-    File['/var/www/html/custom_404.html'],
-    File['/var/www/html/index.html'],
-  ],
+# Restart Nginx to apply the changes
+exec { 'Restarting nginx':
+  command => 'sudo service nginx restart',
+  provider => shell,
 }
