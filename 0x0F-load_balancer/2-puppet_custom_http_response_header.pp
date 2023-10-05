58cd8e19668adbd4392871d6a3e787e82d8845e0
Vlone Me <francisntima@gmail.com>
2023-10-05 12:54:31 +0000

    custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,36 +6,6 @@ exec { "Installing nginx":
 	provider => shell
 }
 
-# Redirection Configuration
-exec { 'Redirection Configuration':
-	command => 'sudo sed -i "s|server_name _|server_name _;\\n\\trewrite ^/redirect_me https://github.com/VloneMe permanent|g" /etc/nginx/sites-enabled/default',
-	provider => shell
-}
-
-# Add "Hello, World!" to index.html
-file { '/var/www/html/index.html':
-  	ensure  => 'file',
-  	content => "Hello, World!\n",
-}
-
-# Create a custom 404 Not Found page
-file { '/var/www/html/custom_404.html':
-  	ensure  => 'file',
-  	content => "Ceci n'est pas une page\n",
-}
-
-# Configure Nginx to use the custom 404 page
-exec { 'Nginx Custom Configuration':
-  	command => 'sudo sed -i "s|listen 80 default_server;|listen 80 default_server;\\n\terror_page 404 /custom_404.html;\\n\tlocation = /custom_404.html {\\n\t\troot /var/www/html;\\n\t\tinternal;\\n\t}|g" /etc/nginx/sites-enabled/default',
-  	path    => ['/usr/bin', '/bin'],
-}
-
-# Check Nginx configuration for syntax errors
-exec { 'Check Nginx Configuration':
-  	command => 'sudo nginx -t',
-	provider => shell
-}
-
 # Add a custom HTTP header to the Nginx configuration
 exec { 'HTTP header Response':
 	command => 'sudo sed -i "/server_name _/a add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-enabled/default',
