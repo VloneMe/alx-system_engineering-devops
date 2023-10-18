291bacf8d44149f2b87d8357e511f5fa1395c6a3
Vlone Me <francisntima@gmail.com>
2023-10-18 02:55:20 +0000

    create user w/replica prev

---
+++ 0x14-mysql/mysql_grant_prev.sql
@@ -0,0 +1,4 @@
+-- Create a MySQL user named holberton_user
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
