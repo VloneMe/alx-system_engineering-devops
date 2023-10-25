08e3e7ddb3c855b00278c39d92e607e0e3a9d44c
VloneMe <francisntima@yahoo.com>
2023-10-25 05:01:46 -0700

    test

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-# This script uses a REST API to fetch information about an employee's TODO list progress
-# and exports the data in JSON format.
+# Using what you did in the task #0, extend your Python
+# script to export data in the JSON format.
 
-# Import necessary libraries
 import json
 from requests import get
 from sys import argv
 
-# Define a function to fetch data from the REST API and export it as JSON
-def fetch_and_export_todo_data(employee_id):
+
+# A function to fetch data from the REST API and export it as JSON
+def api_to_json(user_id):
     # Create an empty list to store task information
-    todo_list = []
+    task_list = []
     
     # Define the base URL for the JSONPlaceholder API
-    api_base_url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/"
     
-    # Fetch employee information using the provided employee_id
-    employee_data = get(api_base_url + "users/{}".format(employee_id)).json()
+    # Fetch user information using the provided user_id
+    user = get(url + "users/{}".format(user_id)).json()
     
-    # Fetch TODO tasks associated with the employee
-    tasks = get(api_base_url + "todos?userId={}".format(employee_id)).json()
+    # Fetch tasks associated with the user
+    tasks = get(url + "todos?userId={}".format(user_id)).json()
     
     # Iterate through the tasks and create dictionaries for each task
     for task in tasks:
-        task_info = {}
-        task_info["task"] = task.get("title")
-        task_info["completed"] = task.get("completed")
-        task_info["employee_username"] = employee_data.get("username")
-        todo_list.append(task_info)
+        tdict = {}
+        tdict["task"] = task.get("title")
+        tdict["completed"] = task.get("completed")
+        tdict["username"] = user.get("username")
+        task_list.append(tdict)
     
-    # Open a JSON file with the employee_id as the filename and write the data
-    with open("{}.json".format(employee_id), 'w', newline='') as json_file:
-        json.dump({argv[1]: todo_list}, json_file)
+    # Open a JSON file with the user_id as the filename and write the data
+    with open("{}.json".format(user_id), 'w', newline='') as json_file:
+        json.dump({argv[1]: task_list}, json_file)
+
 
 # Check if the script is being run as the main program
 if __name__ == "__main__":
-    # Call the fetch_and_export_todo_data function with the employee ID provided as a command-line argument
-    fetch_and_export_todo_data(int(argv[1]))
+    # Call the api_to_json function with the user ID
+    # provided as a command-line argument
+    api_to_json(int(argv[1]))
