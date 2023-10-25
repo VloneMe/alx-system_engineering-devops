9a19e5d6696ff12ca74156184cdea4310a7bcd4a
VloneMe <francisntima@yahoo.com>
2023-10-25 04:04:55 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,36 +7,34 @@ import csv
 from requests import get
 from sys import argv
 
-
-def get_data_api(user_id):
+"""
+A function to fetch data from the API and export it to a CSV file
+"""
+def api_to_csv(user_id):
     url = "https://jsonplaceholder.typicode.com/"
     user = get(url + "users/{}".format(user_id)).json()
     tasks = get(url + "todos?userId={}".format(user_id)).json()
 
-    # Create a CSV file with the user's ID as the filename
-    filename = "{}.csv".format(user_id)
-    with open(filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
+    """
+    # Create a new CSV file with the user's ID as the filename
+    """
+    with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
+        file_stream = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-        # Write the header row to the CSV file
-        csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-                )
+        """
+        # Write the CSV header row
+        """
+        file_stream.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
+        """
+        # Iterate through tasks and write each task's information to the CSV file
+        """
         for task in tasks:
-            # Extract task information
-            task_id = task.get("id")
-            task_title = task.get("title")
-            task_completed = task.get("completed")
-
-            # Write task information to the CSV file
-            csv_writer.writerow(
-                    [user_id, user["name"], task_completed, task_title]
-                    )
-
-    print("Data exported to {}.csv".format(user_id))
+            file_stream.writerow([user_id, user.get("username"), task.get("completed"), task.get("title")])
 
 
 if __name__ == "__main__":
-    user_id = int(argv[1])
-    get_data_api(user_id)
+    """
+    # Extract the employee ID from the command-line arguments
+        and call the 'api_to_csv' function """
+    api_to_csv(int(argv[1]))
